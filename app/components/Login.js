6654628e23c60620b90036998e3e686e1ad4de49
zuhxz <zuhxz@gmail.com>
2018-01-24 16:06:44 +0100

    dsad

--- app/components/Login.js
+++ app/components/Login.js
@@ -26,7 +26,7 @@ export default class Login extends Component {
   render() {
     return (
       <div>
-        <h2>Login</h2>
+        <h2>Login yo:</h2>
         <input onChange={this.handleChange} type="text" value={this.state.username} />
         <button onClick={this.handleLogin}>Log In</button>
       </div>
--- app/main.js
+++ app/main.js
@@ -1,11 +1,18 @@
 import path from 'path';
 import url from 'url';
 import {app, crashReporter, BrowserWindow, Menu} from 'electron';
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
 
 const isDevelopment = (process.env.NODE_ENV === 'development');
 
 let mainWindow = null;
 let forceQuit = false;
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
@@ -39,9 +46,14 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', async () => {
+
+autoUpdater.checkForUpdatesAndNotify();
+
   if (isDevelopment) {
     await installExtensions();
   }
+  
+	
 
   mainWindow = new BrowserWindow({ 
     width: 1000, 
@@ -103,4 +115,43 @@ app.on('ready', async () => {
       }]).popup(mainWindow);
     });
   }
+ 
+
 });
+
+// 
+			 function sendStatusToWindow(text) {
+			  log.info(text);
+			//  win.webContents.send('message', text);
+			}
+			function createDefaultWindow() {
+			  win = new BrowserWindow();
+			  win.webContents.openDevTools();
+			  win.on('closed', () => {
+				win = null;
+			  });
+			  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+			  return win;
+			}
+			autoUpdater.on('checking-for-update', () => {
+			  sendStatusToWindow('Checking for update...');
+			})
+			autoUpdater.on('update-available', (info) => {
+			  sendStatusToWindow('Update available.');
+			})
+			autoUpdater.on('update-not-available', (info) => {
+			  sendStatusToWindow('Update not available.');
+			})
+			autoUpdater.on('error', (err) => {
+			  sendStatusToWindow('Error in auto-updater. ' + err);
+			})
+			autoUpdater.on('download-progress', (progressObj) => {
+			  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+			  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+			  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+			  sendStatusToWindow(log_message);
+			})
+			autoUpdater.on('update-downloaded', (info) => {
+			  sendStatusToWindow('Update downloaded');
+			}); 
+//  
